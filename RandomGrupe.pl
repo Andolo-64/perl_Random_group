b044fca1630a95d11b3ea5f532e669bbad3a41ed
Andolo-64 <andreasolavhogestol@gmail.com>
2024-05-18 16:07:47 +0200

    Generates not random Grups

--- RandomGrupe.pl
+++ RandomGrupe.pl
@@ -1,19 +1,76 @@
-#©Andolo 2023
+#©Andolo 2023-2024 All rights reserved
 
 #!/usr/bin/perl
+korMangeGruper();
+
+sub korMangeGruper
+{
+    print "Hvor mange gruper skal du lage?\n";
+    our $GetGrupenomer = <STDIN>;
+    ChekGrupenomer();
+}
+
+sub ChekGrupenomer
+{
+    if ($GetGrupenomer =~  /^-?\d+$/)
+    {
+        our $Grupenomer = int($GetGrupenomer);
+    }
+    else
+    {
+        print "skriv kor mange gruper du vil ha med et tall\n";
+
+        korMangeGruper();
+    }
+}
+
+inputFolkIGrupe();
+
+sub inputFolkIGrupe
+{
 
 my $text = "Skriv in alle som skal vere i gruper\n";
 
 print "$text";
 
-our $Grupe = <STDIN>;
+my $Grupe = <STDIN>;
 
 our @GrupeNavn = split / /, $Grupe;
 
+our $KormangeiGrupeNavn = scalar @GrupeNavn;
 
-print "ny Grupe\n";
+chekOmGrupeErLiten();
+}
 
-foreach my $SplitGrupe (@GrupeNavn) {
+sub chekOmGrupeErLiten
+{
+    if($KormangeiGrupeNavn >= $Grupenomer)
+    {
 
-    print "$SplitGrupe\n";
+    } 
+    else
+    {
+        print "Du har for lite navn til og generere en så stor grupe\n";
+        inputFolkIGrupe();
+    }
+}
+
+print "$KormangeiGrupeNavn  $Grupenomer ";
+
+$GrupeStorelse = $KormangeiGrupeNavn / $Grupenomer;
+print "$GrupeStorelse \n";
+
+our $GrupeIndex = 0;
+
+for(my $i = 1;$i <= $Grupenomer;$i++)
+{
+
+    print "Grupe$i\n";
+    for (my $i;$i < $GrupeStorelse;$i++)
+    {
+
+        $SplitGrupe = @GrupeNavn[$GrupeIndex]; 
+        print "$SplitGrupe\n";
+        $GrupeIndex++;
+    }
 }
